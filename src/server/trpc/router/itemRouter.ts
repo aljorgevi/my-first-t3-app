2a05cc64a5d69eb3139cf54065502ffe746f7bcb
aljorgevi <jormencar@yahoo.com>
2022-11-19 21:16:41 +0000

    add first mutation route working  ✅

---
+++ src/server/trpc/router/itemRouter.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+import { router, publicProcedure } from "../trpc";
+
+export const itemRouter = router({
+  addItem: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(({ input, ctx }) => {
+      return ctx.prisma.shoppingItem.create({
+        data: {
+          name: input.name,
+        },
+      });
+    }),
+});
+
+/*
+export const exampleRouter = router({
+  hello: publicProcedure
+    .input(z.object({ text: z.string().nullish() }).nullish())
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input?.text ?? "world"}`,
+      };
+    }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.example.findMany();
+  }),
+});
+*/
